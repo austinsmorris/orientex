e8cfaf11b556bc40186e4e73c883e537c85ff3c0
Austin Morris <austin.morris@gmail.com>
2016-07-28 19:53:00 -0400

    update mix file for 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Orientex.Mixfile do
   def project do
     [
       app: :orientex,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
