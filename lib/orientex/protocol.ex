b676f46c242cb15bb86a699400f91145feb65f5a
Austin Morris <austin.morris@gmail.com>
2016-08-08 16:51:32 -0400

    roll 0.0.6

--- lib/orientex/protocol.ex
+++ lib/orientex/protocol.ex
@@ -83,7 +83,7 @@ defmodule Orientex.Protocol do
       {:byte, Request.get_operation_value(request)}, # operation
       nil, # initial session_id
       {:string, "Orientex"}, # driver name
-      {:string, Keyword.get(Orientex.Mixfile.project, :version)}, # driver version
+      {:string, Keyword.get(Mixfile.project, :version)}, # driver version
       {:short, @protocol_version}, # protocol version
       {:string, ""}, # client id
       {:string, "ORecordSerializerBinary"}, # serialization implementation
