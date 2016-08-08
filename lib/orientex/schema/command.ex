3ec7581ea65addae56f8f050ad40a996ab75814d
Austin Morris <austin.morris@gmail.com>
2016-08-08 15:11:11 -0400

    create initial command schema

--- lib/orientex/schema/command.ex
+++ lib/orientex/schema/command.ex
@@ -4,6 +4,14 @@ defmodule Orientex.Schema.Command do
   # todo - this is an implementation of a behavior or protocol or some interface thingy
   def get_schema() do
     # todo - encapsulate/abstract away the standard response header
-    [:byte, :int, :byte, {:int, [:short, :byte, :short, :long, :int, :record]}, :byte]
+    [:byte, :int, :byte, &get_schema_for_result_type/1]
+  end
+
+  def get_schema_for_result_type(108) do # 108 is the byte for "l"
+    [{:int, [:short, :byte, :short, :long, :int, :record]}, :byte]
+  end
+
+  def get_schema_for_result_type(119) do # 119 is the byte for "w"
+    [[:short, :byte, :short, :long, :int, :record], :byte]
   end
 end
---
+++ test/orientex/schema/command_test.exs
@@ -0,0 +1,25 @@
+defmodule Orientex.Schema.CommandTest do
+  use ExUnit.Case
+
+  alias Orientex.Schema.Command
+
+  test "the proper schema is returned" do
+    [a, b, c, d] = Command.get_schema()
+    assert a == :byte
+    assert b == :int
+    assert c == :byte
+    assert is_function(d) == true
+  end
+
+  test "the proper schema is returned for list of records" do
+    [a, b, c, d] = Command.get_schema()
+    schema = d.(108)
+    assert schema == [{:int, [:short, :byte, :short, :long, :int, :record]}, :byte]
+  end
+
+  test "the proper schema is returned for wrapped single record" do
+    [a, b, c, d] = Command.get_schema()
+    schema = d.(119)
+    assert schema == [[:short, :byte, :short, :long, :int, :record], :byte]
+  end
+end
