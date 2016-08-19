bb05bdf4aa9bb987ddbe3cd597b593761e9e9316
Austin Morris <austin.morris@gmail.com>
2016-08-19 12:56:14 -0400

    roll 0.0.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Orientex.Mixfile do
   def project do
     [
       app: :orientex,
-      version: "0.0.7",
+      version: "0.0.8",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
