988a35e7aaf8ae23314d2c2dae5487779c18e162
Austin Morris <austin.morris@gmail.com>
2016-10-10 16:27:11 -0400

    scaffold Orientex.Types.RecordTest

---
+++ test/orientex/types/record_test.exs
@@ -0,0 +1,12 @@
+defmodule Orientex.Types.RecordTest do
+  use ExUnit.Case
+
+#  alias Orientex.Types.Record
+
+#   test "decode boolean false with trailing data" do
+#     {:ok, value, tail} = Types.decode(:boolean, <<0, 4>>)
+#     assert value == false
+#     assert tail == <<4>>
+#   end
+
+end
