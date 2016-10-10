2b34c8bdec36befbb6e6de1cd41ecc3c9e5489aa
Austin Morris <austin.morris@gmail.com>
2016-10-10 19:44:54 -0400

    cleanup Schema tests

--- test/orientex/schema_test.exs
+++ test/orientex/schema_test.exs
@@ -3,24 +3,25 @@ defmodule Orientex.SchemaTest do
 
   alias Orientex.Schema
 
-  test "the proper schema is returned for :request_connect" do
-    schema = Schema.get_schema_for_request(:request_connect)
+  test "Schema struct" do
+    schema = %Schema{}
     assert schema.progress == []
     assert schema.result == []
+    assert schema.schema == []
+  end
+
+  test "the proper schema is returned for :request_connect" do
+    schema = Schema.get_schema_for_request(:request_connect)
     assert schema.schema == Schema.Connect.get_schema()
   end
 
   test "the proper schema is returned for :request_db_open" do
     schema = Schema.get_schema_for_request(:request_db_open)
-    assert schema.progress == []
-    assert schema.result == []
     assert schema.schema == Schema.DbOpen.get_schema()
   end
 
   test "the proper schema is returned for :request_command" do
     schema = Schema.get_schema_for_request(:request_command)
-    assert schema.progress == []
-    assert schema.result == []
     assert schema.schema == Schema.Command.get_schema()
   end
 end
