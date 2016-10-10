b8c95a78c96a7f69cc282b5400a07dbec6e47607
Austin Morris <austin.morris@gmail.com>
2016-10-10 14:02:33 -0400

    cleanup warnings for Orientex.Types and add data type values

--- lib/orientex/types.ex
+++ lib/orientex/types.ex
@@ -1,6 +1,9 @@
 defmodule Orientex.Types do
   @moduledoc false
 
+  alias Orientex.Document
+  alias Orientex.Types.Record
+
   # Decode!
 
   # boolean
@@ -30,15 +33,6 @@ defmodule Orientex.Types do
   end
   def decode(:binary, tail), do: {:incomplete, nil, tail}
 
-  def decode_binary(-1, data), do: {:ok, nil, data}
-  def decode_binary(length, data) when byte_size(data) >= length do
-    <<value :: bytes-size(length), tail :: binary>> = data
-    {:ok, value, tail}
-  end
-  def decode_binary(length, data) do
-    {:incomplete, nil, <<length :: signed-size(32), data :: binary>>}
-  end
-
   # compound types
   # todo - test compound types
   def decode({count_type, template}, data) when is_list(template)do
@@ -49,8 +43,27 @@ defmodule Orientex.Types do
         {:incomplete, nil, tail}
     end
   end
-  def decode({count_type, template}, value, data) when is_list(template) do
+
+  def decode(template, data) when is_list(template) do
+    case do_decode_list(template, data, []) do
+      {:ok, value, tail} ->
+        {:ok, value, tail}
+      {:incomplete, _, _} ->
+        {:incomplete, nil, data}
+    end
+  end
+
+  def decode({_count_type, template}, value, data) when is_list(template) do
     decode_compound_data(template, value, data)
+  end
+
+  def decode_binary(-1, data), do: {:ok, nil, data}
+  def decode_binary(length, data) when byte_size(data) >= length do
+    <<value :: bytes-size(length), tail :: binary>> = data
+    {:ok, value, tail}
+  end
+  def decode_binary(length, data) do
+    {:incomplete, nil, <<length :: signed-size(32), data :: binary>>}
   end
 
   def decode_compound_data(_, {count, values} = value, tail) when count == length(values) do
@@ -65,26 +78,17 @@ defmodule Orientex.Types do
     end
   end
 
-  def decode(template, data) when is_list(template) do
-    case do_decode_list(template, data, []) do
-      {:ok, value, tail} ->
-        {:ok, value, tail}
-      {:incomplete, _, _} ->
-        {:incomplete, nil, data}
-    end
-  end
-
   defp do_decode_list([type | []], data, acc) do
     case decode(type, data) do
       {:ok, value, tail} ->
         {:ok, acc ++ [value], tail}
-      {:incomplete, nil, tail} -> {:incomplete, acc, data}
+      {:incomplete, nil, _tail} -> {:incomplete, acc, data}
     end
   end
   defp do_decode_list([type | type_tail], data, acc) do
     case decode(type, data) do
       {:ok, value, tail} -> do_decode_list(type_tail, tail, acc ++ [value])
-      {:incomplete, nil, tail} -> {:incomplete, acc, data}
+      {:incomplete, nil, _tail} -> {:incomplete, acc, data}
     end
   end
 
@@ -119,14 +123,40 @@ defmodule Orientex.Types do
 
   # bytes and strings
   def encode({:bytes, value}) when is_binary(value), do: encode({:binary, value})
+  def encode({:string, nil}), do: encode({:string, ""})
   def encode({:string, value}) when is_binary(value), do: encode({:binary, value})
   def encode({:binary, value}) when is_binary(value), do: encode({:int, byte_size(value)}) <> value
-
-  # todo - record
-  # def encode({:record, value}), do:
 
   # strings
   def encode({:strings, value}) when is_list(value) do
     encode({:int, length(value)}) <> Enum.reduce(value, <<>>, fn(string, acc) -> acc <> encode({:string, string}) end)
   end
+
+  # todo - record
+  def encode(%Document{} = record), do: Record.encode(record)
+
+  def get_data_type(:boolean), do: 0
+  def get_data_type(:int), do: 1
+  def get_data_type(:short), do: 2
+  def get_data_type(:long), do: 3
+  def get_data_type(:float), do: 4
+  def get_data_type(:double), do: 5
+  def get_data_type(:datetime), do: 6
+  def get_data_type(:string), do: 7
+  def get_data_type(:binary), do: 8
+  def get_data_type(:embedded), do: 9
+  def get_data_type(:embedded_list), do: 10
+  def get_data_type(:embedded_set), do: 11
+  def get_data_type(:embedded_map), do: 12
+  def get_data_type(:link), do: 13
+  def get_data_type(:link_list), do: 14
+  def get_data_type(:link_set), do: 15
+  def get_data_type(:link_map), do: 16
+  def get_data_type(:byte), do: 17
+  def get_data_type(:transient), do: 18
+  def get_data_type(:date), do: 19
+  def get_data_type(:custom), do: 20
+  def get_data_type(:decimal), do: 21
+  def get_data_type(:link_bag), do: 22
+  def get_data_type(:any), do: 23
 end
