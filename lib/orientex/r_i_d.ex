c93cf0ffb47ccc7d20275d0445f8b2f5169767a7
Austin Morris <austin.morris@gmail.com>
2016-10-17 14:18:57 -0400

    implement String.Chars for Orientex.RID

--- lib/orientex/r_i_d.ex
+++ lib/orientex/r_i_d.ex
@@ -3,3 +3,9 @@ defmodule Orientex.RID do
 
   defstruct cluster_id: nil, cluster_position: nil
 end
+
+defimpl String.Chars, for: Orientex.RID do
+  def to_string(%Orientex.RID{cluster_id: cluster_id, cluster_position: cluster_position}) do
+    "##{cluster_id}:#{cluster_position}"
+  end
+end
